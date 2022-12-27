891c9891c9d7649397ad43a98a1ff09a5074c2db
mertc <mert.can58@hotmail.de>
2022-12-27 22:36:03 +0100

    meilenstein 5

---
+++ src/views/test.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1>Welcome to Persons</h1>
+  <div class="container-fluid">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      <div class="col" v-for="person in persons" :key="person.id">
+        <div class="card h-100">
+          <img :src="getAvatar(person)" class="card-img-top" :alt="person.firstName + ' ' + person.lastName">
+          <div class="card-body">
+            <h5 class="card-title">{{ person.firstName }} {{ person.lastName }}</h5>
+            <p class="card-text">
+              {{ person.firstName }} {{ person.lastName }} ist {{ person.vaccinated ? 'geimpft' : 'nicht geimpft' }} und
+              hat {{ person.pets.length }} Haustier(e).
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Persons',
+  data () {
+    return {
+      persons: []
+    }
+  },
+  methods: {
+    getAvatar (person) {
+      if (person.gender === 'MALE') {
+        return require('../assets/male.png')
+      } else if (person.gender === 'FEMALE') {
+        return require('../assets/female.png')
+      }
+    }
+  },
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/persons'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(person => {
+        this.persons.push(person)
+      }))
+      .catch(error => console.log('error', error))
+  }
+}
+</script>
+
+<style scoped>
+</style>
